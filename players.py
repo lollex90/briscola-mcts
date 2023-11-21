603baf2d84abc1530377110909288f59ed499dcb
lollex90 <j.m.terlikowski@lse.ac.uk>
2023-11-21 12:36:30 +0000

    added my mcts player

--- players.py
+++ players.py
@@ -1,7 +1,8 @@
 import random
 from games4e import monte_carlo_tree_search, alpha_beta_search, alpha_beta_cutoff_search
-from tools import compare_cards, get_card_value, evaluation_function
-
+from tools import compare_cards, get_card_value, my_monte_carlo_tree_search, generate_move_smart
+from copy import deepcopy   
+full_deck = [(i, suit) for i in ['Ace', '2', '3', '4', '5', '6', '7', 'Jack', 'Horse', 'King'] for suit in ['Bastoni', 'Denari', 'Spade', 'Coppe']]
 
 class RandomPlayer:
     """A player that chooses a legal move at random."""
@@ -9,7 +10,6 @@ class RandomPlayer:
     def generate_move(self, state, game):
         """Choose a random move from the given legal moves."""
         return random.choice(game.actions(state))
-
 
 class HumanPlayer:
     """A player that chooses a legal move based on user input."""
@@ -23,21 +23,18 @@ class HumanPlayer:
 
         # Print the player's hand and the table
         print("Your hand: ", state_copy[f"hand{self.number}"])
-        print("Table: ", state_copy["table"])
 
-        # Ask the user to pick 0, 1 or 2 and check if the input is valid
+        # Ask the user to pick a card and check if the input is valid
         while True:
-            move_index = input("Pick a card (0, 1 or 2): ")
-            if move_index in ["0", "1", "2"]:
-                break
-            else:
+            move = input("Pick a card: ")
+            move = eval(move)
+            if move not in full_deck:
                 print("Invalid input, try again.")
-
-        move = state_copy[f"hand{self.number}"][int(move_index)]
+            else:
+                break
 
         # Return the move
         return move
-
 
 class SmartPlayer:
     """A player that chooses a legal move based on the briscola strategy.
@@ -66,17 +63,35 @@ class SmartPlayer:
             # return the move with the highest evaluation
             return moves[moves_eval.index(max(moves_eval))]
 
+# class MonteCarloTreeSearchPlayer:
+#     """A player that chooses a legal move based on the Monte Carlo Tree Search algorithm."""
 
-class MonteCarloTreeSearchPlayer:
+#     def __init__(self, simulations=1000):
+#         """Initialize the player."""
+#         self.simulations = simulations  # the number of simulations
+
+#     def generate_move(self, state_copy, game):
+#         """Choose a move based on the Monte Carlo Tree Search algorithm."""
+#         if len(state_copy["taken1"]) + len(state_copy["taken2"]) < 40:
+#             return generate_move_smart(state_copy, game)
+#         else:
+#             return monte_carlo_tree_search(state_copy, game, self.simulations)
+        
+class MyMonteCarloTreeSearchPlayer:
     """A player that chooses a legal move based on the Monte Carlo Tree Search algorithm."""
 
     def __init__(self, simulations=1000):
         """Initialize the player."""
         self.simulations = simulations  # the number of simulations
 
-    def generate_move(self, state_copy, game):
+    def generate_move(self, state, game):
         """Choose a move based on the Monte Carlo Tree Search algorithm."""
-        return monte_carlo_tree_search(state_copy, game, self.simulations)
+        # solve fully last 3 tricks
+        state_copy = deepcopy(state)    
+        if len(state_copy["taken1"]) + len(state_copy["taken2"]) < 34:
+            return my_monte_carlo_tree_search(state_copy, game, self.simulations)
+        else:
+            return alpha_beta_search(state_copy, game)
     
 class AlphaBetaPlayer:
 
@@ -94,7 +109,9 @@ class AlphaBetaPruningPlayer:
         # self.d = d
         pass
     
-    def generate_move(self, state_copy, game):
+    def generate_move(self, state_single, game):
         """Choose a move based on alpha beta search with pruning"""
-        return alpha_beta_cutoff_search(state_copy, game)
+        # take a single player state and randomly assign a hand to the other player
+
+        return alpha_beta_cutoff_search(state_single, game)
 
